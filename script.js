02b1f464c66544bc3b971c3a8c8369c50d247b7a
Kauri Nanda <bknanda6@gmail.com>
2020-01-25 23:54:19 -0700

    updating html, css, js files

--- script.js
+++ script.js
@@ -87,6 +87,22 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
     }
 };
 
+// countdown timer
+function MyTimer(callback, val) {
+    val = val || 60; 
+    var timer=setInterval(function() { 
+        callback(val);
+        if(val-- <= 0) { 
+            clearInterval(timer); 
+        } 
+    }, 1000);
+}
+new MyTimer(function(val) {
+    var timerMsg = "00:" + (val >= 10 ? val : "0" + val);
+    document.getElementById("timer").textContent = timerMsg; 
+});
+
+
 
 // use object literals to respresent indiv questions
 // use array to hold all questions that make up quiz
